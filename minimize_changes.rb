922f6e977eea237baec4ccdb8cf19986fe9e553b
hrigu <chmuehlethaler@freesurf.ch>
2014-01-13 13:46:39 +0100

    Minizinc für Output verwendet

--- minimize_changes.rb
+++ minimize_changes.rb
@@ -3,6 +3,7 @@
 require 'erb'
 require 'nokogiri'
 require_relative 'lib/minimize_changes'
+require 'fileutils'
 
 def out_info text
   puts "###################"
@@ -37,20 +38,41 @@ employee_number = 4
 system("cd files/created && rm loesung.txt")
 
 # generiert flatzinc, smt, erste Lösung: alles um schlussendlich das File zu lösen
-system( "cd files/created && ~/dienstplan/trunk/solver/solver_master/processing.sh #{mzn_file}")
-system( "cd files/created && mpirun -n 5 ~/dienstplan/trunk/solver/solver_master/squeeze_ubuntu_12.04 --solver-name yices-smt --solver-path $(dirname $(which yices-smt)) --employee-number #{employee_number} --global-timeout 10 --solver-timeout 2 --oi end_liegenlassen.txt --os beste_loesung.txt -i #{mzn_file} -s loesung.txt -g grenzen_modifiziert.txt --statistic statistic.txt")
+system("cd files/created && ~/dienstplan/trunk/solver/solver_master/processing.sh #{mzn_file}")
+system("cd files/created && mpirun -n 5 ~/dienstplan/trunk/solver/solver_master/squeeze_ubuntu_12.04 --solver-name yices-smt --solver-path $(dirname $(which yices-smt)) --employee-number #{employee_number} --global-timeout 10 --solver-timeout 2 --oi end_liegenlassen.txt --os beste_loesung.txt -i #{mzn_file} -s loesung.txt -g grenzen_modifiziert.txt --statistic statistic.txt")
 
 
 # Lösung lesen
-
 out_info "Beste Lösung"
 
+solution = nil
 File.open("files/created/beste_loesung.txt", "r") do |infile|
   while (line = infile.gets)
+    solution = line
     puts "#{line}"
   end
 end
 
 
+out_info "Loesung schreiben "
+
+tempfile = File.open("files/created/#{mzn_file}.tmp", "w")
+File.open("files/created/#{mzn_file}", "r") do |infile|
+  while (line = infile.gets)
+    tempfile << line
+    if line =~ /^%hook_for_show_solution/
+      tempfile.puts "%Die Lösung\n"
+      tempfile.puts solution
+      tempfile.puts "\n"
+    end
+  end
+
+end
+
+tempfile.close
+FileUtils.mv("files/created/#{mzn_file}.tmp", "files/created/#{mzn_file}")
+
+
+system("cd files/created && minizinc #{mzn_file}")
 
 out_info "FERTIG"
